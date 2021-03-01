0cdd8eda898b1b8b62699dc6c3d25ee8525265c4
ncclymer <77338319+ncclymer@users.noreply.github.com>
2021-03-01 10:02:54 -0700

    Add files via upload
    initial commit from local repository.

---
+++ script.js
@@ -0,0 +1,18 @@
+function makePassword(){
+  let complexity = document.getElementById("card-body")
+}
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
