3bee75ebd0f9cdbead728d0b0f32c5f7367579ff
ncclymer <nick_clymer2018@yahoo.com>
2021-03-06 20:46:01 -0700

    Updated script by removing debugging tools, added screenshot, and README

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # 03-ncclymer-password-generator
+
+    This project is intended to create a secure random password to make the process of selecting a new password
+simpler for the user. We hope to add a copy button feature and to streamline the user variable input experience.
+
+1. When a user clicks on the button a series of prompts for user input begins. Complete
+2. The criteria for character selection includes: uppercase, lowercase, numbers, and special characters. Complete
+3. The password output should only contain the types of characters that the user has selected. Complete
+4. The generated password should be displayed in the HTML. Complete
+
+
+Application and GIT repository URL's:
+
+Site URL: https://ncclymer.github.io/03-ncclymer-password-generator/
+
+Git Repo URL: https://github.com/ncclymer/03-ncclymer-password-generator
+
+Screen shot: ![Porfolio Image](03-screenshot.png)
--- script.js
+++ script.js
@@ -1,39 +1,41 @@
+// Character array source.
 var uppercase_char_codes = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var lowercase_char_codes = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var number_char_codes = ["0","1","2","3","4","5","6","7","8","9"];
 var special_char_codes = ["&","!","#","$","%"];
 
+// Random selection generator.
 var randomChoice = function(param) {
   var i = Math.floor(Math.random() * param.length);
   var computerChoice = param[i];
   return computerChoice
 }
 
-var passwordLength = prompt("Enter a number between 7 and 129");
-
+// Random choice target array for output.
 var newArray = [];
 
+// User information, and variable data collection function.
 var getUserInput = function() {    
-
+  
+    let generatedPassword = ""
+    var passwordLength = prompt("Enter a number between 7 and 129");
     var passwordLower = prompt("Would you like to use lowercase letters? y/n");
+
     if (passwordLower == "y") {
       var lowercase = randomChoice(lowercase_char_codes)
       newArray.push(lowercase);
-      console.log(lowercase)
     }
 
     var passwordUpper = prompt("Would you like to use uppercase letters? y/n");
     if (passwordUpper == "y") {
       var uppercase = randomChoice(uppercase_char_codes)
       newArray.push(uppercase);
-      console.log(uppercase)
     }
 
     var passwordNumber = prompt("Would you like to use numbers? y/n");
     if (passwordNumber == "y") {
       var number = randomChoice(number_char_codes)
       newArray.push(number);
-      console.log(number)
     }
 
     var passwordSpecial = prompt("Would you like to use special characters? y/n");
@@ -41,29 +43,16 @@ var getUserInput = function() {
       var special = randomChoice(special_char_codes)
       var specialString = special.toString();
       newArray.push(specialString);
-      console.log(special)
+     
+    for (var i=0; i < passwordLength; i++) { 
+      let datRando = randomChoice(newArray)
+      generatedPassword += datRando
+      document.getElementById("password").innerHTML = generatedPassword;
     } 
+  }
 }
 
-getUserInput()
-console.log(newArray)
+var generateBtn = document.querySelector("#generate");
+// User initiation call.
+generateBtn.addEventListener("click", getUserInput);
 
-var generatePassword = function(newArray) {
-  let generatedPassword = ""
-  for (var i=0; i < passwordLength; i++) { 
-  let datRando = randomChoice(newArray)
-  generatedPassword += datRando
-  document.getElementById("password").innerHTML = generatedPassword;
- }
-  console.log(generatedPassword)
- 
-}
-generatePassword(newArray)
-
-  
-
-  
-  // Assignment Code
-  var generateBtn = document.querySelector("#generate");
-  
-  generateBtn.addEventListener("click", getUserInput);
